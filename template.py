8e631eebd882e03a0e6176bad7acec5e9a2359f1
jugesdebnath7 <jugesdebnath7@gmail.com>
2025-04-06 23:18:18 +0530

    Project Structure Updated

--- template.py
+++ template.py
@@ -18,6 +18,8 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/tests/__init__.py",
+    f"src/{project_name}/tests/test.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
